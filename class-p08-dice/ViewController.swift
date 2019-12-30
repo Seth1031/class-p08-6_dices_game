0324921e15f31774b070de4467b22249195aa3eb
Seth <skeror@gmail.com>
2019-12-30 19:22:44 +0800

    Initial Commit

---
+++ class-p08-dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  class-p08-dice
+//
+//  Created by Seth on 2019/12/30.
+//  Copyright © 2019 Seth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ class-p08-diceTests/class_p08_diceTests.swift
@@ -0,0 +1,34 @@
+//
+//  class_p08_diceTests.swift
+//  class-p08-diceTests
+//
+//  Created by Seth on 2019/12/30.
+//  Copyright © 2019 Seth. All rights reserved.
+//
+
+import XCTest
+@testable import class_p08_dice
+
+class class_p08_diceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
